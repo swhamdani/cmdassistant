0b7da7f6f7f3d78f9240d3f4722ff46c6c4b3074
Wasif <47280616+swhamdani@users.noreply.github.com>
2019-02-05 18:25:43 +0500

    Initial commit

---
+++ CMDAssistant/CMDAssistant/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CMDAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.resultlbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.commandlbl = new System.Windows.Forms.Label();
+            this.commandtitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.commandbox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Executing CMD Commands using C#";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get MAC ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(23, 159);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 39);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Hostname ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(23, 40);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 39);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "IP Configuration";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // resultlbl
+            // 
+            this.resultlbl.AutoSize = true;
+            this.resultlbl.Location = new System.Drawing.Point(180, 43);
+            this.resultlbl.Name = "resultlbl";
+            this.resultlbl.Size = new System.Drawing.Size(37, 13);
+            this.resultlbl.TabIndex = 11;
+            this.resultlbl.Text = "Result";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.commandlbl);
+            this.groupBox1.Controls.Add(this.commandtitle);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.commandbox);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 555);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Command";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(331, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(134, 23);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Execute on CMD Prompt";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // commandlbl
+            // 
+            this.commandlbl.AutoSize = true;
+            this.commandlbl.Location = new System.Drawing.Point(8, 114);
+            this.commandlbl.Name = "commandlbl";
+            this.commandlbl.Size = new System.Drawing.Size(37, 13);
+            this.commandlbl.TabIndex = 19;
+            this.commandlbl.Text = "Result";
+            // 
+            // commandtitle
+            // 
+            this.commandtitle.AutoSize = true;
+            this.commandtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandtitle.Location = new System.Drawing.Point(8, 82);
+            this.commandtitle.Name = "commandtitle";
+            this.commandtitle.Size = new System.Drawing.Size(121, 15);
+            this.commandtitle.TabIndex = 18;
+            this.commandtitle.Text = "Command Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Enter Command";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Execute Here";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // commandbox
+            // 
+            this.commandbox.Location = new System.Drawing.Point(92, 40);
+            this.commandbox.Name = "commandbox";
+            this.commandbox.Size = new System.Drawing.Size(141, 20);
+            this.commandbox.TabIndex = 18;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Controls.Add(this.resultlbl);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(518, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(628, 555);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Most Used Commands";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(23, 415);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(130, 39);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Driver Query";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(23, 480);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(130, 38);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Cipher";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(23, 286);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 39);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Task List";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(23, 351);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(130, 38);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Stored Users Info";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(23, 101);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 39);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "System Info";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 685);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Command Prompt Assistant";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label resultlbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox commandbox;
+        private System.Windows.Forms.Label commandtitle;
+        private System.Windows.Forms.Label commandlbl;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ CMDAssistant/CMDAssistant/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CMDAssistant
+{
+    public partial class Form1 : Form
+    {
+        string finalResult = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            commandlbl.Hide();
+            commandtitle.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             resultlbl.Text = ExecuteCommand("getmac");     
+        }
+           
+        private void button5_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("ipconfig"); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("hostname"); 
+        }
+
+        public string ExecuteCommand(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                procStartInfo.WorkingDirectory = @"C:\";
+                // Now we create a process, assign its ProcessStartInfo and start it
+                Process proc = new Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                
+                
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                //Console.WriteLine(result);  
+                
+                /*var lineRead = Regex.Split(result, "\r\n|\r|\n");
+                for(int i=0; i<lineRead.Length; i++)
+                {
+                    if (lineRead[i].Contains("Maximum password age"))
+                    {
+                        finalresultlbl.Text += "\n" + lineRead[i];
+                    }
+
+                    if (lineRead[i].Contains("Password last set"))
+                    {
+                        finalresultlbl.Text += "\n"+lineRead[i];
+                    }
+                }*/
+
+                return result;
+            }
+            catch (Exception objException)
+            {
+                return "Exception: "+ objException;
+            }
+        }      
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(commandbox.Text.Length >= 2)
+            {
+                commandlbl.Visible = true;
+                commandtitle.Visible = true;
+                commandlbl.Text = ExecuteCommand(commandbox.Text);                
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid command");
+            }
+        }
+
+        private void runCommand(string command)
+        {
+            var proc1 = new ProcessStartInfo();
+            string Command;
+            proc1.UseShellExecute = true;
+            Command = @command;
+            proc1.WorkingDirectory = @"C:\";//C:\Windows\System32
+            proc1.FileName = @"C:\Windows\System32\cmd.exe";
+            // as admin = proc1.Verb = "runas";
+            proc1.Arguments = "/k " + Command;
+            proc1.WindowStyle = ProcessWindowStyle.Maximized;
+            Process.Start(proc1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (commandbox.Text.Length >= 2)
+            {
+                runCommand(commandbox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid command");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("Systeminfo");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("Tasklist"); 
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("Cmdkey");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("Driverquery");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            resultlbl.Text = ExecuteCommand("Cipher"); 
+        }
+    }
+}
